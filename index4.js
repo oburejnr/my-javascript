c6edf76cb0ba74e5c10c9f275d9b2ba58d217cf2
oburejnr <cynthiaobure293@gmail.com>
2023-02-12 23:25:45 +0300

    javascript

---
+++ index4.js
@@ -0,0 +1,13 @@
+function addEvenNumbersBetween0and10(){
+     let sum = 0
+    for(let j = 0; j < 11; j++){
+        if (j % 2 == 0) {
+            //console.log(number);
+            sum += j
+
+        }
+
+    }
+console.log(sum);
+}
+addEvenNumbersBetween0and10
